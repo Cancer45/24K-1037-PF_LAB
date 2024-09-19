615e827b8a52f3c9ad00b2171076b4a1c72c1a46
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-09-19 15:43:48 +0500

    Add files via upload

---
+++ LAB5/task3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+   char type;
+   int b_time = , w_time;
+   printf("Input 'W' for White Coffeee and 'B' for Black Coffee");
+   scanf(" %c", &type);
+   
+   switch (type)
+   {
+       case 'W':
+       
+       case 'B':
+   }
+} 

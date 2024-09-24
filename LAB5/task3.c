9330bb149c9242a0cf9eff8ea8eef5bd281ee528
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-09-24 15:03:38 +0500

    Update task3.c

--- LAB5/task3.c
+++ LAB5/task3.c
@@ -2,15 +2,44 @@
 
 int main() {
     
-   char type;
-   int b_time = , w_time;
-   printf("Input 'W' for White Coffeee and 'B' for Black Coffee");
+   char type, size;
+   int t_time, c-size;
+   printf("Input 'W' for White Coffee and 'B' for Black Coffee");
    scanf(" %c", &type);
+   printf("'R' is 100 ml and 'D' is 200 ml\n");
+   printf("Input 'R' for regular, Input 'D' for Double or Input 'M' to manually select size");
+   scanf(" %c", &size);
+   
+    switch (size)
+   {
+       case 'R':
+       c_size = 100;
+       
+       case 'D':
+       c_size = 200;
+       
+       case 'M':
+       printf("Input Cup Size in ml: ");
+       scanf("%d", &c_size);
+       
+   }
    
    switch (type)
    {
        case 'W':
+        printf("");
+        printf("");
+        printf("");
+        printf("");
+        printf("");
+        printf("");
        
        case 'B':
+        printf("");
+        printf("");
+        printf("");
+        printf("");
+        printf("");
+        printf("");
    }
 } 

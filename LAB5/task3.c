cefdf7a1af46b199e5631c027d600ee15c851938
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-09-26 00:49:21 +0500

    Update task3.c

--- LAB5/task3.c
+++ LAB5/task3.c
@@ -5,54 +5,82 @@ int main() {
     // inputs
    char type, size;
    int  c_size;
-   // time values
-   float t_time, t1 = 2, t2 = 5;
+   // time values and factor
+   float factor, t_time, t1 = 2, t2 = 5;
     // white or black?
-   printf("Input 'W' for White Coffee and 'B' for Black Coffee");
+   printf("Input 'W' for White Coffee and 'B' for Black Coffee\n");
    scanf(" %c", &type);
     // cup size?
    printf("'R' is 100 ml and 'D' is 200 ml\n");
-   printf("Input 'R' for regular, Input 'D' for Double or Input 'M' to manually select size");
+   printf("Input 'R' for regular, 'D' for Double or 'M' to manually enter size\n");
    scanf(" %c", &size);
    
     // select cup size
     switch (size)
    {
        case 'R':
+       case 'r':
        c_size = 100;
+       break;
        
        case 'D':
+       case 'd':
        c_size = 200;
+       break;
        
        case 'M':
-       printf("Input Cup Size in ml: ");
+       case 'm':
+       printf("Input Cup Size in ml (must be greater than 100 ml)\n");
        scanf("%d", &c_size);
+       break;
+       
+       default:
+       printf("You didn't select a valid option :(\nTry Again");
+       return 1;
+   }
+       
+    // check c_size
+    if (c_size < 100)
+    {
+        printf("Cup Size must be greater than 100 ml\nTry Again");
+        return 1;
+    }
+    
     //adjusting times according to cup size
     if (c_size != 100)
     {
-        
+        factor = (1 + (0.5 * ((c_size / 100) - 1)));
+        t1 *= factor;
+        t2 *= factor;
     }
     
     // display times for each step
-   }
    
    switch (type)
    {
        case 'W':
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        printf("");
+       case 'w':
+        printf("Adding Water... (%.0f mins)\n", (3 * t2));
+        printf("Adding Sugar... (%.0f mins)\n", (3 * t2));
+        printf("Mixing... (%.0f mins)\n", (4 * t2));
+        printf("Adding Coffee... (%.0f mins)\n", (t1));
+        printf("Adding Milk... (%.0f mins)\n", (2 * t1));
+        printf("Mixing... (%.0f mins)\n", (4 * t2));
+        break;
        
        case 'B':
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        printf("");
-        printf("");
+       case 'b':
+        printf("Adding Water... (%.0f mins)\n", (4 * t2));
+        printf("Adding Sugar... (%.0f mins)\n", (4 * t2));
+        printf("Mixing... (%.0f mins)\n", (5 * t2));
+        printf("Adding Coffee... (%.0f mins)\n", (3 * t2));
+        printf("Mixing... (%.0f mins)\n", (5 * t2));
+        break;
+        
+        default:
+        printf("You didn't select a valid option :(\nTry Again");
+        return 1;
    }
    
+   return 0;
 } 

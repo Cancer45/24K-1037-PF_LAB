e4c751b957c636b97744d2ff62fc2a095374992c
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-10-24 12:31:01 +0500

    Update task5.c

--- LAB7/task5.c
+++ LAB7/task5.c
@@ -13,31 +13,7 @@ int main() {
 
     for (int i = 0; i < 100; i++)
     {
-        //skip if counted
-        if (m != 0 && input[m] == input[0])
-        {
-            m++;
-            n = 0;
-        }
         
-        else
-        {
-            if (input[m] == input[n])
-            {
-                count++;
-                input[n] = input[0];
-            }
-        
-            //print
-            if (n == 9)
-            {
-                printf("frequency of %d: %d\n", input[m], count);
-                count = 0;
-                n = 0;
-                m++;
-            }
-        }
-        n++;
     }
     return 0;
 }

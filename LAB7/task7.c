c8bdcc450cbcc64a5808a5810197c474c603b0c3
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-10-25 00:14:08 +0500

    Update task7.c

--- LAB7/task7.c
+++ LAB7/task7.c
@@ -4,17 +4,60 @@ int main() {
     
     //init array
     char input[100];
-    int result = 0;
+    int tmpnum, result = 0, length = 0, place = 1, num = 0;
+    int check1, check2;
+    
     
     //take input
     printf("input expression to be computed (only use '+' and '-' operators): ");
-    scanf("%s", &input);
+    scanf("%s", input);
     
-    //calculate edpression
-    for (int i = 0; input[i] != '\0'; i++)
+    //length of expression
+    while (input[length] != '\0')
     {
+        length++;
+    }
+    
+    check1 = input[length - 1];
+    check2 = input[0];
+    
+    //check if first or last is operator
+    if ((check1 <= 47 || check1 >= 58) || (check2 <= 47 || check2 >= 58))
+    {
+        printf("ERROR! expression must begin and end with a number");
+        return 1;
+    }
+    
+    //calculate expression
+    for (int i = length - 1; i >= 0; i--)
+    {   
+        //if number
+        if (input[i] > 47 && input[i] < 58)
+        {
+            tmpnum = input[i] - 48;
+            num += tmpnum * place;
+            place *= 10;
+        }
+        
+        //if operator
+        else if (input[i] == 43)
+        {
+            result += num;
+            num = 0;
+            place = 1;       
+        }
+        
+        else if (input[i] == 45)
+        {
+            result -= num;
+            num = 0;
+            place = 1;
+        }
         
     }
-
+    
+    //add last number
+    result += num;
+    printf("%d", result);
     return 0;
 }

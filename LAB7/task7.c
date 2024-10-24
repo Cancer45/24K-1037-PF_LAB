83024d79b19a92786c2f5f8f667dfbf922a8d28b
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-10-24 20:56:26 +0500

    Create task7.c

---
+++ LAB7/task7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    //init array
+    char input[100];
+    int result = 0;
+    
+    //take input
+    printf("input expression to be computed (only use '+' and '-' operators): ");
+    scanf("%s", &input);
+    
+    //calculate edpression
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        
+    }
+
+    return 0;
+}

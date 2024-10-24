1285b81c4d9ea6c1000eb304b851d876f9e67fef
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-10-24 13:11:09 +0500

    Update task5.c

--- LAB7/task5.c
+++ LAB7/task5.c
@@ -10,10 +10,37 @@ int main() {
         scanf("%d", &input[i]);    
     }
     
-
-    for (int i = 0; i < 100; i++)
+    //while index for current count is within array boundaries
+    for (; m < 10;)
     {
+        //if not first index, and first value, skip
+        if ((m != 0) && (input[m] == input[0]))
+        {
+            m++;
+            n = m;
+            continue;
+        }
         
+        //change to first value to siginify counted
+       if(input[m] == input[n])
+       {
+           //only change if not current so that it doesn't skip over current value
+           if (n != m)
+           {
+               input[n] = input[0];
+           }
+           count++;
+       }
+       
+       //end of array at 9, reset values
+       if(n == 9)
+       {
+           printf("frequency of %d is %d\n", input[m], count);
+           count = 0;
+           n = m;
+           m++;
+       }
+       n++;
     }
     return 0;
 }

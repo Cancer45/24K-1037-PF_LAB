e75f5b5dcb267032a4d6eb68ff240b5e54649892
Cancer45 <108489654+Cancer45@users.noreply.github.com>
2024-11-21 15:03:14 +0500

    Add files via upload

---
+++ LAB11/task1/task1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+    char buffer [100];
+    FILE* fptr1 = fopen("dummy.txt", "r");
+    FILE* fptr2 = fopen("new.txt", "w");
+
+    if (fptr1 == NULL || fptr2 == NULL)
+    {
+        return 1;
+    }
+
+    while(fscanf(fptr1, "%s", buffer) != EOF)
+    {
+        fprintf(fptr2, "%s ", buffer);    
+    }
+
+    fclose(fptr1);
+    fclose(fptr2);
+}
